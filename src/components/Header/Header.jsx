796522c931d2285ea5863618568d156bd80a1438
vova3500 <41364337+vova3500@users.noreply.github.com>
2020-12-26 13:52:21 +0300

    start project

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "./header.scss";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="header-title">WeatherApp</div>
+    </header>
+  );
+};
+
+export default Header;
